e24bb18c31c214d1f9b81c4f74852d301a4db58d
Tyler Carter <cartert@rsd407.org>
2022-12-09 06:41:38 -0800

    menu

---
+++ Assets/Scripts/Menu&Saving/Game.cs
@@ -0,0 +1,127 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+  
+    [SerializeField]
+    private GameObject menu;
+    [SerializeField]
+    private GameObject progress;
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        Pause();
+    }
+
+ 
+    public void Pause()
+    {
+        menu.SetActive(true);
+        Cursor.visible = true;
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void Unpause()
+    {
+        menu.SetActive(false);
+        Cursor.visible = false;
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public bool IsGamePaused()
+    {
+        return isPaused;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Unpause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void SaveGame()
+    {
+        // 1
+        Save save = CreateSaveGameObject();
+
+        // 2
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+      
+        Debug.Log("Game Saved");
+    }
+
+    public void LoadGame()
+    {
+        // 1
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+
+            // 2
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+
+            // 3
+            foreach (GameObject g in save.alive)
+            {
+                g.SetActive(true);
+            }
+
+          
+
+            Debug.Log("Game Loaded");
+
+            Unpause();
+        }
+        else
+        {
+            Debug.Log("No game saved!");
+        }
+    }
+
+    public void SaveAsJSON()
+    {
+        Save save = CreateSaveGameObject();
+        string json = JsonUtility.ToJson(save);
+
+        Debug.Log("Saving as JSON: " + json);
+    }
+
+    private Save CreateSaveGameObject()
+    {
+        Save save = new Save();
+        int i = 0;
+        foreach (GameObject ob in progress.transform)
+        {
+            if (ob.activeSelf)
+            {
+                save.alive.Add(ob);
+                i++;
+            }
+        }
+
+        return save;
+    }
+}
